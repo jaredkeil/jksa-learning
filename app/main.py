e3771f10fa6fc68b9ce0aa1dfa6ce4442ffe8cff
Jared Keil <jaredrkeil@gmail.com>
2022-12-17 12:27:49 -0800

    add cors middleware that allows origins defined in settings

--- app/main.py
+++ app/main.py
@@ -1,6 +1,5 @@
-import os
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .core.config import settings
 from .database import create_db_and_tables
@@ -9,6 +8,13 @@ from .init_db import create_first_superuser, dummy_data
 
 app = FastAPI(title='JKSA Learning')
 app.include_router(api_router)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=settings.BACKEND_CORS_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.on_event('startup')
