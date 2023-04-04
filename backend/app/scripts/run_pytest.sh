8c575b1ed5ccafe5d8e2938e7243f82a43d0dbe6
Jared Keil <jaredrkeil@gmail.com>
2023-04-04 13:41:37 -0700

    Removing coverage report from pytest run

--- backend/app/scripts/run_pytest.sh
+++ backend/app/scripts/run_pytest.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-set -e
 echo "User: $(whoami)"
 export COVERAGE_FILE=/app/cov/.coverage
 . $(poetry env info --path)/bin/activate
-#pytest --cov=app app/tests
+
 set -x
-pytest --cov=app --cov-report=term-missing app/tests "${@}"
+#pytest --cov=app --cov-report=term-missing app/tests "${@}"
+pytest app/tests "${@}"
