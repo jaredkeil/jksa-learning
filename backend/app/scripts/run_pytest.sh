4a53722c537a62d58e4d299363b890caa2b9f925
Jared Keil <jaredrkeil@gmail.com>
2023-04-04 11:55:45 -0700

    Update Dockerfile and run_pytest for pytest coverage permissions

--- backend/app/scripts/run_pytest.sh
+++ backend/app/scripts/run_pytest.sh
@@ -2,8 +2,6 @@
 
 set -e
 echo "User: $(whoami)"
-mkdir -p /app/cov
-chown -R app:app /app/cov
 export COVERAGE_FILE=/app/cov/.coverage
 . $(poetry env info --path)/bin/activate
 #pytest --cov=app app/tests
