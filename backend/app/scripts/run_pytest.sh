7f6917cd228432153a4d3fe5e4c4eee9edfe9a14
Jared Keil <jaredrkeil@gmail.com>
2023-04-04 11:48:43 -0700

    Update test.yml and run_pytest

--- backend/app/scripts/run_pytest.sh
+++ backend/app/scripts/run_pytest.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 set -e
-set -x
-
+echo "User: $(whoami)"
+mkdir -p /app/cov
+chown -R app:app /app/cov
+export COVERAGE_FILE=/app/cov/.coverage
+. $(poetry env info --path)/bin/activate
 #pytest --cov=app app/tests
+set -x
 pytest --cov=app --cov-report=term-missing app/tests "${@}"
